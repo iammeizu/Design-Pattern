98b607f51cb768807ff74209e5700b1e9c80d99d
shi <shixu@megvii.com>
2018-11-13 10:27:49 +0800

    flyweight to be continue

---
+++ by_Python/structural-pattern/flyweight.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+import random
+from enum import Enum
+
+TreeType = Enum('TreeType', 'apple_tree cherry_tree peach_tree')
+
+
+class Tree:
+    pool = dict()
+
+    def __new__(cls, tree_type):
+        obj = cls.pool.get(tree_type, None)
+        if not obj:
+            obj = object.__new__(cls)
+            cls.pool[tree_type] = obj
+            obj.tree_type = tree_type
+        return obj
+
+    def render(self, age, x, y):
+        print('render a tree of type {} and age {} at ({}, {})'.format(self.tree_type, age, x, y))
+        
